e0924b77129c7a43c66096528aa9d2f137b81303
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-28 23:11:23 +0530

    service

--- src/loose/EmailNotificationService.java
+++ src/loose/EmailNotificationService.java
@@ -1,7 +1,6 @@
 package loose;
 
-public class EmailNotificationService implements  NotificationService{
-
+public class EmailNotificationService implements NotificationService {
     @Override
     public void send(String message) {
         System.out.println("Email: " + message);
